dac1c366c1d394c9505985646abb7f2d5f2ee888
Pradyumna Kattel <126577744+pradyumna14@users.noreply.github.com>
2024-02-01 19:13:12 +0000

    done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmail = "Pradyumnakattel14@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+
+//accountId = 2
+//console.log(accountId);  // Not allowed to change the constant. it will throw an error!
+
+accountPassword = "1111111"
+accountEmail = "23bcs131@gmail.com"
+accountCity = "Bangalore"
+
+console.table([accountPassword, accountId, accountEmail, accountCity, accountState]);
+
+
