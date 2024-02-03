3977c3a5a1ac69ae00e29c1653ba97d0be8a24c4
[pradyumna14] <pradyumnakattel14@gmail.com>
2024-02-03 19:24:17 +0545

    Done with data types

---
+++ 02_dataTypes.js
@@ -0,0 +1,11 @@
+// we dont have to mention the 
+// data type like that we do in the C language, 
+// like int A = 2;
+// we have to instead just write
+// let A = 2; this automatically understands the 
+// int data type
+
+console.log(typeof(null)); // null is an object not a type! null ko data type is object!
+console.log(typeof(undefined)); // type of undefined is undefined itself!
+// this above 2 typeof things is an interview questions!!
+ 
