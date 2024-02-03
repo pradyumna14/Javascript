bda9c875689765ec8f3fd2f1944c69590b8567f3
[pradyumna14] <pradyumnakattel14@gmail.com>
2024-02-04 01:43:11 +0545

    done

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,27 @@
+// data types are 2 types, primitive and non primitive
+
+//primitive data tyoes 7 ota hunxan and all are
+// call by value(copy hunxa value) original nai change hudaina!
+
+/*
+7 types are: String, Numbers, Boolean, null, undefined, symbol, BigInt
+
+REFERENCE TYPE/NON PRIMITIVE
+: Array, Objects, Functions
+
+*/
+// JS is a Dynamic typed language as we donot have to specify the data type!
+
+//Symbol
+const id = Symbol('123');
+const anotherId = Symbol('123');
+console.log(id==anotherId);
+// >> answer is false, although 123 is same in both case, we have defined symbol as the data type soo different auxai auxa
+// >> symbol is used for uniqueness like this only!!!
+
+/*
+
+REFERENCE TYPE/NON PRIMITIVE
+: Array, Objects, Functions
+
+*/
