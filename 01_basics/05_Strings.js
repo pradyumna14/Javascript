900265d5289ce49c43593990b283c09b9e3202b6
[pradyumna14] <pradyumnakattel14@gmail.com>
2024-02-05 13:38:49 +0545

    doing

--- 01_basics/05_Strings.js
+++ 01_basics/05_Strings.js
@@ -1,5 +1,11 @@
 const name = "Pradyumna"
 const repoCount = 50
 
-console.log("Hello my name is"+ name + " and my repo count is " + repCount);
+console.log("Hello my name is "+ name + " and my repo count is " + repoCount);
+// this is not a good practise to be done!!!
 
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+// this is a good practice
+
+// HOW TO DECLARE A STRING
+console.log(typeof("hello"));
