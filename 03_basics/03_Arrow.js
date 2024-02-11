8fe7b482274e55f9c3685961fc08bc08d43bdd24
[pradyumna14] <pradyumnakattel14@gmail.com>
2024-02-12 00:09:05 +0530

    done

--- 03_basics/03_Arrow.js
+++ 03_basics/03_Arrow.js
@@ -20,3 +20,21 @@ function chai(){
 }
 chai()
 */
+
+
+// ARROW FUNCTION+++++++
+
+const addTwo = (num1, num2) => {
+    return num1+num2
+}
+console.log(addTwo(3,4))
+// return key word use garda, explicit return
+// return keyword use nagarnu parda implicit return hunxa.
+const add = (num1, num2) =>  num1+num2
+
+console.log(add(3,4))
+
+
+const add3 = (num1, num2) =>  ({username: "Pradyumna"})  // yesari object banauda ta compulsorily round bracket ko use garnu parney nai hunxa!.
+console.log(add3(3,4))
+
